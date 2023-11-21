5550916b527d17fb3e442417bb3ac0f89000a599
rohith-sagar <rohith.karnala@modak.com>
2023-11-21 15:36:06 +0530

    updated

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='yeedu_operator_test',
-    version='0.0.2',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         'apache-airflow>=2.7.3',
         # Add any other dependencies here
     ],
+    project_urls={
+        'GitHub': 'https://github.com/rohith-sagar/AirflowYeeduOperator.git',
+        # Add more URLs as needed
+    },
 )
 
